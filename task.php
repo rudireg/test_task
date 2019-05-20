a8b511d683209532952daf35be6cb099b0c8d598
Rudi <rudireg@ya.ru>
2019-05-20 23:47:03 +0300

    Update task.php

--- task.php
+++ task.php
@@ -14,3 +14,28 @@ function randUniqueGenerator (): array
         };
         return array_chunk(array_keys($temp),1000);
     }
+
+
+/**
+* Проверка баланса строки
+**/
+public function isBalanced(string $input)
+    {
+        $pairs = [
+           '{' => '}',
+           '[' => ']',
+           '(' => ')'
+        ];
+        $opens = [];
+        $symbols = str_split($input);
+        foreach ($symbols AS $s) {
+           if (in_array($s, array_keys($pairs))) {
+               $opens[] = $s;
+               continue;
+           }
+           if (empty($opens)) return false;
+           $o = array_pop($opens);
+           if ($s !== $pairs[$o]) return false;
+        };
+        return empty($lemmas);
+    }
