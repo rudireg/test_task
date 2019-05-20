7c855e59b2cf03e877deee0841d622cad598df39
Rudi <rudireg@ya.ru>
2019-05-20 22:54:02 +0300

    Update task.php

--- task.php
+++ task.php
@@ -3,20 +3,14 @@
 /**
 * Генерация чисел. При условии достаточной памяти
 **/
-function randUniqueGenerator (): array 
+function randUniqueGenerator (): array
     {
         $min = 20000;
         $max = 2000000;
         $total = 1000 * 1000;
-        $result = $temp = array();
+        $temp = array();
         while (sizeof($temp) < $total) {
             $temp[mt_rand($min, $max)] = true;
         };
-        $temp = array_keys($temp);
-        for ($i=0; $i<1000; $i++) {
-            for ($b = 0; $b < 1000; $b++) {
-                $result[$i][$b] = array_shift($temp);
-            }
-        }
-        return $result;
+        return array_chunk(array_keys($temp),1000);
     }
